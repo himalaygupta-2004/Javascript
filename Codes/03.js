3899faac9db53e78a1899acfe36e0a6f1d834c33
Himalay Gupta <141752020+himalaygupta-2004@users.noreply.github.com>
2024-03-11 15:32:17 +0000

    learned about type conversions

---
+++ Codes/03.js
@@ -0,0 +1,21 @@
+//typeof func 
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+/* ------------------------------Operations-------------------------------------- */
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//strict check ===
+// checks both datatypes and values
+
+
+
