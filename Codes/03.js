47d31008ca70d3b9268208e130f6f37b00af32f5
Himalay Gupta <141752020+himalaygupta-2004@users.noreply.github.com>
2024-04-04 03:46:00 +0000

    typeof func

--- Codes/03.js
+++ Codes/03.js
@@ -17,5 +17,24 @@ console.log(typeof stringNumber);
 //strict check ===
 // checks both datatypes and values
 
+//primitive - 7 : call by value.
+// types - Strings , numbers, booleans, NULL, undefined, symbol, BigInt
+
+//reference type : non primitive
+// array, objects, functions,
+
+//js is dynamically typed.
+//const heros = ["shaktiman","nagraj","chota bheem"];
+let myObj = {
+    name : himalay,
+    age : 20,
+}
+console.log(myObj);
+const myFunc = function () {
+    console.log("hello world");
+}
+
+//typeof func used to find data type of variables.
+
 
 
