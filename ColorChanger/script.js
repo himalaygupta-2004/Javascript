70a9b505c9fc557fe2a18d149432f34ef7a53b65
Himalay Gupta <141752020+himalaygupta-2004@users.noreply.github.com>
2024-07-07 00:21:36 +0530

    Add files via upload
    ColorChanger Projecy

---
+++ ColorChanger/script.js
@@ -0,0 +1,25 @@
+const button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+//buttons ke upar we can also function via loop
+//or else event
+
+//event listners ko use krenge humlog
+
+button.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    console.log(event.target);
+    if (event.target.id === "grey") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "white") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "blue") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "yellow") {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
+});
